da362feb5bd24d4fe303ee30e84ab346285edd6d
adityasingh6967 <kpsingh50151@gmail.com>
2026-03-03 17:34:26 +0530

    dashboard layout

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -2,6 +2,10 @@ import DashboardShell from "./DashboardShell";
 
 export const metadata = {
   title: "GetMeABoost - Dashboard",
+  description: "GetMeABoost - This website is a crowdfunding platform for creators.",
+  icons: {
+    icon: "logo_dark.png"
+  }
 };
 
 export default function DashboardLayout({ children }) {
