bed9adb7fa137ae84cb71e8a0b9e2775d30b3de3
adityasingh6967 <kpsingh50151@gmail.com>
2026-03-03 17:36:07 +0530

    discord login setup

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import Discord from "next-auth/providers/discord";
 import Google from "next-auth/providers/google";
 
 const authOptions = {
