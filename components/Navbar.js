ba21470b74d0b895af480a1486579b3765c475f7
adityasingh6967 <kpsingh50151@gmail.com>
2026-03-08 12:40:55 +0530

    hmm.

--- components/Navbar.js
+++ components/Navbar.js
@@ -61,7 +61,7 @@ const Navbar = () => {
   }
 
   return (
-    <nav className="py-2 px-1 sm:px-15 fixed top-0 z-10 w-full bg-[#191919] flex justify-center items-center">
+    <nav className="py-2 px-1 sm:px-15 fixed top-0 z-100000000000 w-full bg-[#191919] flex justify-center items-center">
       <div className="w-full flex justify-between items-center">
         <Link href="/">
           <Image
